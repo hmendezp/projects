d2a9a1067fa12761bbb6bc994724bc87e7114e99
Homero <homero.mendez@gmail.com>
2013-01-24 18:45:14 -0600

    Update Fraction.h

--- Fraction.h
+++ Fraction.h
@@ -2,12 +2,15 @@
 
 @interface Fraction : NSObject
 {
-  int numerator, denominator;
+  int sign, numerator, denominator;
 }
-@property int numerator, denominator;
+@property int sign, numerator, denominator;
 - (void) print;
 - (void) setTo: (int) n over: (int) d;
 - (double) convertToNum;
 - (Fraction *) add: (Fraction *) f;
+- (Fraction *) subtract: (Fraction *) f;
+- (Fraction *) multiply: (Fraction *) f;
+- (Fraction *) divide: (Fraction *) f;
 - (void) reduce;
 @end
